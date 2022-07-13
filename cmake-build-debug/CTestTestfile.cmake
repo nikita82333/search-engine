9538ebeba96929eacc3da2681808ac7aa842ff6a
Кузнецов Никита Вениаминович <kunv@kbl.nsk.su>
2022-07-13 21:37:47 +0700

    Remove ignored files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kunv/CLionProjects/search_engine
-# Build directory: C:/Users/kunv/CLionProjects/search_engine/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/kunv/CLionProjects/search_engine/cmake-build-debug/search_engine[1]_include.cmake")
-subdirs("_deps/json-build")
-subdirs("_deps/googletest-build")
